2e0a6ccf1d2a8322fcbd88b3c07ed19a8fca9e0f
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2015-08-09 15:36:48 +0300

    Add arraylist and arraylist test

---
+++ datastructures/arraylist.go
@@ -0,0 +1,60 @@
+package ctci
+
+import (
+	"fmt"
+)
+
+type ArrayList struct {
+	list       []int
+	size       int
+	last_index int
+}
+
+// get the length of the list
+func (arr *ArrayList) Len() int {
+	return len(arr.list)
+}
+
+// get all the elements
+func (arr *ArrayList) GetElements() []int {
+	return arr.list
+}
+
+// print all the elements
+func (arr *ArrayList) PrintElements() {
+	for idx, elem := range arr.list {
+		fmt.Println("index:", idx, "element:", elem)
+	}
+}
+
+// get element of given index
+func (arr *ArrayList) Get(idx int) int {
+	if idx < arr.Len() {
+		return arr.list[idx]
+	} else {
+		return -1
+	}
+}
+
+// add int element to the list
+func (arr *ArrayList) Set(elem int) {
+	// reaches the limit of underlying array
+	if int(arr.size) == arr.last_index {
+
+		new_part := make([]int, arr.size*2)
+		fmt.Println()
+		copy(new_part, arr.list)
+		arr.list = new_part
+		arr.size = arr.size * 2
+
+	}
+	arr.list[arr.last_index] = elem
+	arr.last_index++
+}
+
+// initialize the arraylist structure
+func (arr *ArrayList) Init(size int) {
+	// init underlying list of arraylist
+	arr.size = size
+	arr.list = make([]int, arr.size)
+}
---
+++ datastructures/arraylist_test.go
@@ -0,0 +1,105 @@
+package ctci
+
+import (
+	"testing"
+)
+
+func TestLen(t *testing.T) {
+	l := ArrayList{}
+	l.Init(10)
+
+	// test initial size
+	if l.Len() != 10 {
+		t.Error("For length",
+			"Expected", 10,
+			"got", l.Len())
+	}
+	for i := 0; i < 15; i++ {
+		l.Set(i)
+	}
+	// test new size
+	if l.Len() != 20 {
+		t.Error("For length",
+			"Expected", 20,
+			"got", l.Len())
+	}
+
+	for i := 0; i < 15; i++ {
+		l.Set(i)
+	}
+	// test new size
+	if l.Len() != 40 {
+		t.Error("For length",
+			"Expected", 40,
+			"got", l.Len())
+	}
+
+}
+
+func TestSet(t *testing.T) {
+	l := ArrayList{}
+	l.Init(10)
+	// set to idx=0
+	l.Set(10)
+	// set to idx=1
+	l.Set(13)
+	// set to idx=2
+	l.Set(15)
+
+	if l.Get(0) != 10 {
+		t.Error("For length",
+			"Expected", 10,
+			"got", l.Get(0))
+	}
+	if l.Get(1) != 13 {
+		t.Error("For length",
+			"Expected", 13,
+			"got", l.Get(1))
+	}
+	if l.Get(2) != 15 {
+		t.Error("For length",
+			"Expected", 15,
+			"got", l.Get(2))
+	}
+
+}
+func TestGet(t *testing.T) {
+	l := ArrayList{}
+	l.Init(10)
+
+	// set to idx=0
+	l.Set(10)
+
+	if l.Get(0) != 10 {
+		t.Error("For length",
+			"Expected", 10,
+			"got", l.Get(0))
+	}
+
+}
+func TestGetElements(t *testing.T) {
+
+	l := ArrayList{}
+	l.Init(10)
+	// set to idx=0
+	l.Set(10)
+	// set to idx=1
+	l.Set(13)
+
+	if elem := l.GetElements()[0]; elem != 10 {
+		t.Error("For length",
+			"Expected", 10,
+			"got", elem)
+	}
+	if elem := l.GetElements()[1]; elem != 13 {
+		t.Error("For length",
+			"Expected", 13,
+			"got", elem)
+	}
+	if elem := l.GetElements()[2]; elem != 0 {
+		t.Error("For length",
+			"Expected", 0,
+			"got", elem)
+	}
+
+}
