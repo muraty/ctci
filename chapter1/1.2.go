6bee6e8d081bc8f0ce7d45bcfb74283525c02815
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2015-08-13 21:39:57 +0300

    Add question definition

--- chapter1/1.2.go
+++ chapter1/1.2.go
@@ -1,5 +1,8 @@
 package chapter1
 
+// Implement a function void reverse(char* str) in C or C++ which
+// reverses a null- terminated string.
+
 // reverses given string pointer
 func reverse(s *string) {
 
