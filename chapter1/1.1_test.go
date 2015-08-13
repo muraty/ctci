ad96d321bc620f69c82be19e54cf313bafffdb31
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2015-08-13 21:06:28 +0300

    Change test variables

--- chapter1/1.1_test.go
+++ chapter1/1.1_test.go
@@ -4,19 +4,19 @@ import (
 	"testing"
 )
 
-type testPair struct {
+type IsAllUniqueTestPair struct {
 	s      string
 	result bool
 }
 
-var tests = []testPair{
+var isAllUniqueTests = []IsAllUniqueTestPair{
 	{"murat", true},
 	{"yildirim", false},
 	{"12345678a~'`ZXVBNM[]{}:?<>", true},
 }
 
 func TestIsAllUnique(t *testing.T) {
-	for _, test := range tests {
+	for _, test := range isAllUniqueTests {
 		if elem := IsAllUnique(test.s); elem != test.result {
 			t.Error("For ", test.s,
 				"Expected", test.result,
